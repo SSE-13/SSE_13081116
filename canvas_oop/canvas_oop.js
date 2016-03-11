926950443a81802f9d968849c377e217e3f0a4ab
smibatah <1255640109@qq.com>
2016-03-11 17:57:15 +0800

    交了三天都没交上的作业= =

--- canvas_oop/canvas_oop.js
+++ canvas_oop/canvas_oop.js
@@ -1,36 +1,34 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 /**
  * 基类，负责处理x,y,rotation 等属性
  */ 
-class DisplayObject {
-
-    x = 0;
-
-    y = 0;
- 
-    rotation = 0;
-
-    draw(context: CanvasRenderingContext2D) {
-        context.save(); 
+var DisplayObject = (function () {
+    function DisplayObject() {
+        this.x = 0;
+        this.y = 0;
+        this.rotation = 0;
+    } 
+    DisplayObject.prototype.draw = function (context) {
+        context.save();
         context.rotate(this.rotation);
         context.translate(this.x, this.y);
-        this.render(context); 
-
+        this.render(context);
         context.restore();
+    };
+    DisplayObject.prototype.render = function (context) {
+    };
+    return DisplayObject;
+}());
+var Bitmap = (function (_super) {
+    __extends(Bitmap, _super);
+    function Bitmap() {
+        _super.apply(this, arguments);
     }
-
-    render(context: CanvasRenderingContext2D) {
-
-    }
-
-}
-
-class Bitmap extends DisplayObject {
-
-
-    source;
-
-    render(context: CanvasRenderingContext2D) {
-
+    Bitmap.prototype.render = function (context) {
         var image = imagePool[this.source];
         if (image) {
             context.drawImage(image, 10, 10);
@@ -40,50 +38,50 @@ class Bitmap extends DisplayObject {
             context.fillStyle = '#000000';
             context.fillText('错误的URL', 0, 20);
         }
+    };
+    return Bitmap;
+}(DisplayObject));
+var Rect = (function (_super) {
+    __extends(Rect, _super);
+    function Rect() {
+        _super.apply(this, arguments);
+        this.width = 100;
+        this.height = 100;
+        this.color = '#FF0000';
     }
-
-}
-
-class Rect extends DisplayObject {
-
-    width = 100
-
-    height = 100;
-
-    color = '#FF0000';
-
-    render(context: CanvasRenderingContext2D) {
+    Rect.prototype.render = function (context) {
         context.fillStyle = this.color;
         context.fillRect(0, 0, this.width, this.height);
+    };
+    return Rect;
+}(DisplayObject));
+var TextField = (function (_super) {
+    __extends(TextField, _super);
+    function TextField() {
+        _super.apply(this, arguments);
     }
-}
-
-class TextField extends DisplayObject {
     //color='#FFFFFF';
-    render(context: CanvasRenderingContext2D) {
+    TextField.prototype.render = function (context) {
         context.font = "50px Agency FB Bold";
         context.fillStyle = '#FFFFFF';
         context.fillText('FlappyBird', 0, 100);
-    }
-}
-
+    };
+    return TextField;
+}(DisplayObject));
 function drawQueue(queue) {
     for (var i = 0; i < renderQueue.length; i++) {
-        var displayObject: DisplayObject = renderQueue[i];
+        var displayObject = renderQueue[i];
         displayObject.draw(context);
     }
 }
-
 var imagePool = {};
-
 function loadResource(imageList, callback) {
     var count = 0;
-    imageList.forEach(function(imageUrl) {
+    imageList.forEach(function (imageUrl) {
         var image = new Image();
         image.src = imageUrl;
         image.onload = onLoadComplete;
         image.onerror = onLoadError;
-
         function onLoadComplete() {
             imagePool[imageUrl] = image;
             count++;
@@ -91,17 +89,13 @@ function loadResource(imageList, callback) {
                 callback();
             }
         }
-        
-        function onLoadError(){
+        function onLoadError() {
             alert('资源加载失败:' + imageUrl);
         }
-    })
+    });
 }
-
-
-var canvas: HTMLCanvasElement = document.getElementById("game") as HTMLCanvasElement;
+var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
-
 /*
 var rect = new Rect();
 rect.width = 70;
@@ -110,27 +104,19 @@ rect.x=40;
 rect.y=300;
 rect.color = '#3e9602'
 */
-
 var text = new TextField();
 text.x = 30;
-
-
 var bitmap = new Bitmap();
 bitmap.source = 'wander-icon.jpg';
-
 var bitmap2 = new Bitmap();
 bitmap2.source = '1.png';
-bitmap2.x=100;
-bitmap2.y=150;
-
+bitmap2.x = 100;
+bitmap2.y = 150;
 //渲染队列
-var renderQueue = [bitmap,bitmap2,text];
+var renderQueue = [bitmap, bitmap2, text];
 //资源加载列表
-var imageList = ['wander-icon.jpg','1.png'];
-
+var imageList = ['wander-icon.jpg', '1.png'];
 //先加载资源，加载成功之后执行渲染队列
-loadResource(imageList, function() {
+loadResource(imageList, function () {
     drawQueue(renderQueue);
-})
-
-
+});
